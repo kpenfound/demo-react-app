b8ca05ec39c38a9a9e55015cbfa831c6ec8d8f7c
kpenfound <kyle@dagger.io>
2023-08-09 14:14:30 -0400

    Add Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,17 @@
+pipeline {
+  agent { label 'dagger' }
+  
+  environment {
+    _EXPERIMENTAL_DAGGER_CLOUD_TOKEN = "a276ce43-e1ca-4427-a6ee-200d77b85b56"
+  }
+  stages {
+    stage("dagger") {
+      steps {
+        sh '''
+            npm install @dagger.io/dagger@latest
+            dagger run npm run ci
+        '''
+      }
+    }
+  }
+}
--- ci/index.js
+++ ci/index.js
@@ -2,25 +2,36 @@
   let connect = (await import("@dagger.io/dagger")).connect
 
   connect(async (client) => {
-    const image = "index.docker.io/kylepenfound/demo-app:latest"
-    const src = client.host().directory(".", {exclude:["node_modules/"]})
+    // const image = "index.docker.io/kylepenfound/demo-app:latest"
+    const src = client.host().directory(".", {exclude:["node_modules/", "out/", "ci/"]})
 
-    const test = client.container()
-    .from("node:16")
+    const cache = client.cacheVolume("yarn_cache")
+    const npm = client.cacheVolume("node_modules")
+
+    // Test container
+    const test = client.container().pipeline("Test")
+    .from("node:16@sha256:466d0a05ecb1e5b9890960592311fa10c2bc6012fc27dbfdcc74abf10fc324fc")
     .withDirectory("/src", src)
     .withWorkdir("/src")
+    .withMountedCache("/cache/yarn", cache)
+    .withEnvVariable("YARN_CACHE_FOLDER", "/cache/yarn")
+    .withMountedCache("/src/node_modules", npm)
     .withEnvVariable("CI", "true")
-    .withExec(["npm", "install"])
-    .withExec(["npm", "test"])
+    .withExec(["yarn", "install", "--frozen-lockfile"]) // some change abcd
+    .withExec(["yarn", "run", "test"])
 
+    // Test results
     const testResults = await test.stdout()
     console.log(testResults)
 
+    // Production image build
     const build = test.withExec(["npm", "run", "build"])
-    const prod = client.container()
+    const prod = client.container().pipeline("Production Build")
     .from("cgr.dev/chainguard/nginx:latest")
     .withDirectory("/var/lib/nginx/html", build.directory("/src/build"))
 
-    await prod.publish(image)
+    // Production image output
+    await prod.export("./out/prod-image.tar")
+    // await prod.publish(image)
   }, {LogOutput: process.stdout})
 })()
