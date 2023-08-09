e490d0a2f51e3fa5d5cbc5fa865bed726d319afa
kpenfound <kyle@dagger.io>
2023-08-09 16:41:40 -0400

    small application change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello YNA!
+          Edit <code>src/App.js</code> and save to reload. Small Application Change.
         </p>
         <a
           className="App-link"
