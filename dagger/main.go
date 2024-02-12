25afee92590901906275f56092d49b21f6678e34
Kyle Penfound <kyle@dagger.io>
2024-02-12 17:02:03 -0500

    test workflow formatting
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

---
+++ dagger/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type DemoReactApp struct {}
+
+func (m *DemoReactApp) Test(ctx context.Context) error {
+	// Step 1
+	startGroup("Step 1")
+	_, err := dag.Container().From("alpine").WithExec([]string{"echo", "hello world step 1"}).Sync(ctx)
+	if err != nil {
+		return err
+	}
+	endGroup()
+
+	// Step 2
+	startGroup("Step 2")
+	_, err = dag.Container().From("alpine").WithExec([]string{"echo", "hello world step 2"}).Sync(ctx)
+	if err != nil {
+		return err
+	}
+	endGroup()
+	// Step 3
+
+	startGroup("Step 3")
+	_, err = dag.Container().From("alpine").WithExec([]string{"echo", "hello world step 3"}).Sync(ctx)
+	if err != nil {
+		return err
+	}
+	endGroup()
+
+	return nil
+}
+
+func startGroup(name string) {
+	fmt.Printf("::group::%s\n", name)
+}
+
+func endGroup() {
+	fmt.Println("::endgroup::")
+}
